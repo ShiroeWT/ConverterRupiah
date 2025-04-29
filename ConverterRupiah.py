e2d70cd29f3c0a1a19fb70e6b89cb68ed54e3a43
Rafly Adha Prathama <144401868+ShiroeWT@users.noreply.github.com>
2025-04-29 12:14:35 +0700

    Adding yen as supported currency

--- ConverterRupiah.py
+++ ConverterRupiah.py
@@ -6,8 +6,17 @@ cleanned_rupiah = ''.join(char for char in rupiah if char not in string.punctuat
 cleanned_rupiah = ''.join(char for char in cleanned_rupiah if char not in string.ascii_letters)
 cleanned_rupiah = int(cleanned_rupiah)
 
-rupiah_to_usd = cleanned_rupiah / 16789
+print("1. USD")
+print("2. Yen")
+pilihan = int(input("choice : "))
+if pilihan == 1:
+    rupiah_to_usd = cleanned_rupiah / 16789
+    print(rupiah_to_usd, "USD")
+elif pilihan == 2:
+    rupiah_to_yen = cleanned_rupiah / 117
+    print(rupiah_to_yen, "yen")
+else :
+    print("choice not valid, program exiting")
 
-print(rupiah_to_usd, "USD")
 
 
